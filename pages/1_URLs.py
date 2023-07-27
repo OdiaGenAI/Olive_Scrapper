8dc737c8ee6383b7b121d2bc8269d7a36684c4c1
arkamaldeen <kabdurrahmaan28@gmail.com>
2023-07-27 23:43:38 +0530

    updated css

--- pages/1_URLs.py
+++ pages/1_URLs.py
@@ -18,6 +18,9 @@ def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+# Load CSS file
+load_css('styles.css')
+
 # ----- FUNCTIONS -----
 # function to check whether the url is a sitemap or not
 def check_sitemap(url):
